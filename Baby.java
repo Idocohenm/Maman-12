a2042a443e6c8ad87721ad8fc856c4debf0ec9b0
Ido <97910251+Idocohenm@users.noreply.github.com>
2024-11-05 12:03:51 +0200

    Update Baby.java
    Changing function: "isWeightInValidRange"

--- Baby.java
+++ Baby.java
@@ -6,9 +6,9 @@ public class Baby {
 	Weight _birthWeight, _currentWeight;
 	final String DEFAULT_ID = "000000000";
 	final int MONTH_0_TO_2_GAIN_PER_DAY = 30;
-	final int MONTH_2_TO_4_GAIN_PER_MONTH = 750;
-	final int MONTH_4_TO_8_GAIN_PER_MONTH = 500;
-	final int MONTH_8_TO_12_GAIN_PER_MONTH = 250;
+	final int MONTH_2_TO_4_GAIN_PER_DAY = 25;
+	final int MONTH_4_TO_8_GAIN_PER_DAY = 16;
+	final int MONTH_8_TO_12_GAIN_PER_DAY = 8;
 
 	public Baby(String fName, String lName, String id, int day, int month, int year, int birthWeightInGrams) {
 		this._firstName = fName;
@@ -83,50 +83,40 @@ public class Baby {
 	public boolean older(Baby other) {
 		return this._dateOfBirth.before(other._dateOfBirth);
 	}
-
-	public int isWeightInValidRange(Date date) {
-		double curWeightGrams = this._currentWeight.getGrams() + this._currentWeight.getKilos() * 1000;
-		double birthWeightGrams = this._birthWeight.getGrams() + this._birthWeight.getKilos() * 1000;
-		if (date.before(this._dateOfBirth)) //Date is before his birth day, not valid
-			return 1;
-		if (daysSinceEpoch(date) - daysSinceEpoch(this._dateOfBirth) >= 365) //Baby is already 1 years old or more
-			return 2;
-		else {
-			return minimumExpectedWeight(daysSinceEpoch(date) - daysSinceEpoch(this._dateOfBirth) , birthWeightGrams) <= curWeightGrams ? 4 : 3;
-		}//Checks if the expected weight is less or more then current weight
-	}
 	
+	public int isWeightInValidRange(int numOfDays) {
+
+		if(numOfDays <= 365){
+			double curWeightGrams = this._currentWeight.getGrams() + this._currentWeight.getKilos() * 1000;
+			double birthWeightGrams = this._birthWeight.getGrams() + this._birthWeight.getKilos() * 1000;
+			return minimumExpectedWeight(numOfDays, birthWeightGrams) >= curWeightGrams ? 3 : 2;
+		}
+		return 1;
+	}	
 	//------------------------------------------private methods---------------------------------------------------------------------------//
-	//Calculate the days since epoch (0/0/0)
-	private int daysSinceEpoch(Date date) {
-		final int DAYS_IN_MONTH = 30; final int DAYS_IN_YEAR = 365;
-		return date.getDay() + date.getMonth() * DAYS_IN_MONTH + date.getYear() * DAYS_IN_YEAR;
-	}
 	
 	//Calculate the minimum expected weight, given the age of the baby and the born weight
-	private double minimumExpectedWeight(int daysSinceBirth, double birthWeightExpected) {
-
-		birthWeightExpected *= 0.9; 
+	private double minimumExpectedWeight(int daysSinceBirth, double bornWeight) {
 		if (daysSinceBirth <= 7)  
 			// First week of birth
-			return birthWeightExpected;
+			return bornWeight - ( ((bornWeight * 0.1) / 7) * daysSinceBirth) ;
 		if (daysSinceBirth <= 60) { 
-			//between month 0-2, everyday +30grams
-			return birthWeightExpected + (MONTH_0_TO_2_GAIN_PER_DAY * (daysSinceBirth - 7)); 
+			//between days 8-60, everyday +30grams
+			return bornWeight * 0.9 + (MONTH_0_TO_2_GAIN_PER_DAY * (daysSinceBirth - 7)); 
 		}
 		if (daysSinceBirth <= 120) {
-			//month 2-4, every month +750grams
-			return birthWeightExpected + (MONTH_0_TO_2_GAIN_PER_DAY * 60 + 
-					MONTH_2_TO_4_GAIN_PER_MONTH * ((daysSinceBirth - 60) / 30)); 
+			//between days 61-120, everyday +25grams
+			return  bornWeight * 0.9 + (MONTH_0_TO_2_GAIN_PER_DAY * 53) +
+					MONTH_2_TO_4_GAIN_PER_DAY * (daysSinceBirth - 60); 
 		}
 		if (daysSinceBirth <= 240) {
-			//month 4-8, every month +500grams
-			return birthWeightExpected + (MONTH_0_TO_2_GAIN_PER_DAY * 60 + MONTH_2_TO_4_GAIN_PER_MONTH * 2 
-					+ MONTH_4_TO_8_GAIN_PER_MONTH * (daysSinceBirth - 120) / 30);
+			//between days 121-240, every day +16grams
+			return bornWeight * 0.9 + (MONTH_0_TO_2_GAIN_PER_DAY * 53) + 
+					(MONTH_2_TO_4_GAIN_PER_DAY *  60) + MONTH_4_TO_8_GAIN_PER_DAY * (daysSinceBirth - 120);
 		} else {
-			//month 8-12, every month +250grams
-			return birthWeightExpected + (MONTH_0_TO_2_GAIN_PER_DAY * 60 + MONTH_2_TO_4_GAIN_PER_MONTH * 2 + MONTH_4_TO_8_GAIN_PER_MONTH * 4
-					+ MONTH_8_TO_12_GAIN_PER_MONTH * ((daysSinceBirth - 240) / 30)); 
+			//between days 241-365, every day +8grams
+			return bornWeight * 0.9 + (MONTH_0_TO_2_GAIN_PER_DAY * 53) + 
+					(MONTH_2_TO_4_GAIN_PER_DAY *  60) + MONTH_4_TO_8_GAIN_PER_DAY * (120) + MONTH_8_TO_12_GAIN_PER_DAY * (daysSinceBirth - 240); 
 		}
 
 	}
